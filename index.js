5c6aaf1013692393883b7eacdb8c00b0ac1a6536
BrianOnchwari <onchwaribrian6@gmail.com>
2024-03-24 12:48:31 +0300

    Arithimetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+// Given variables
+let num1 = 31;
+let num2 = 2;
+let num3 = 44;
+let num4 = 8;
 
-// Write your code here
+// Creating variable multiply
+let multiply = num1 * num2;
+
+// Creating variable random
+let random = Math.floor(Math.random() * 1000) + 1;
+
+// Creating variable mod
+let mod = num3 % num4;
+
+// Creating variable max
+let numbers_set = [10, 15, 20, 5];
+let max = Math.max(...numbers_set);
+
+// Logging the results
+console.log("multiply =", multiply);
+console.log("random =", random_number);
+console.log("mod =", mod);
+console.log("max =", max_value);
